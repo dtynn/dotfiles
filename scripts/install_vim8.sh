0a684b0fd7c6e198a06c00a9fd92f2433b5981c9
dtynn <dtynn@163.com>
2022-08-18 15:01:10 +0800

    chore: upgrade vim version to v8.1.1719

--- scripts/install_vim8.sh
+++ scripts/install_vim8.sh
@@ -1,10 +1,10 @@
 yum install python3 python3-devel ncurses-devel
 
-wget https://github.com/vim/vim/archive/v8.1.1053.tar.gz
+wget https://github.com/vim/vim/archive/v8.1.1719.tar.gz
 
-tar zxvf v8.1.1053.tar.gz
+tar zxvf v8.1.1719.tar.gz
 
-cd vim-8.1.1053
+cd vim-8.1.1719
 ./configure --enable-multibyte --enable-pythoninterp=yes --enable-python3interp=yes
 make
 make install
